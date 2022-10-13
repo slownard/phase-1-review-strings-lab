675c53b19158355582c387358486bb655980e201
Bernard Legaspi <blegaspi26@gmail.com>
2022-10-13 17:18:49 -0400

    commit tree 388b76d2a0e5f0f5b380b785c250e7c93b3a10df

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code in this file!
+
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+
+const firstInitial = currentUser.charAt(0)
+
+const shortGreeting = `Welcome, ${firstInitial}!`;
+
+
+
+function asdf() {
+    console.log("asdf");
+    let num = 0;
+    num += 1;
+}
+
+asdf();
+
+// ctrl shift ~
